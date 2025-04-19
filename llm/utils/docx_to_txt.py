b332cca9e22f52df3b0f935b22758a74d2818379
Danila Moriakov <d.moriakov@gmail.com>
2025-04-19 20:52:11 +0300

    real docs

---
+++ llm/utils/docx_to_txt.py
@@ -0,0 +1,12 @@
+import pypandoc
+
+import os
+from pathlib import Path
+
+docs_dir = Path('/Users/danila/Study/thesis/llm/docs')
+
+for docx_file in docs_dir.glob('*.docx'):
+    txt_file = docx_file.with_suffix('.txt')
+    output = pypandoc.convert_file(
+        str(docx_file), 'plain', outputfile=str(txt_file)
+    )
