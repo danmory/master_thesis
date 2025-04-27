4f6de62e10b4e3b3ebffa93eef9f0d49751e2b81
Danila Moriakov <d.moriakov@gmail.com>
2025-04-27 17:27:41 +0300

    llama -> gemma, add main py script

---
+++ llm/llm/main.py
@@ -0,0 +1,39 @@
+import os
+from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.llms import LlamaCpp
+
+import rag
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+agreement_path = os.path.abspath(os.path.join(script_dir, '..', 'docs', 'short-term-vacation-lease-agreements', 'agreement_1.txt'))
+model_path_str = os.path.abspath(os.path.join(script_dir, '..', 'models', 'google_gemma-3-27b-it-qat-Q5_K_M.gguf'))
+templates_path = os.path.abspath(os.path.join(script_dir, '..', 'templates'))
+
+with open(agreement_path, 'r') as file:
+    rental_agreement = file.read()
+
+model = LlamaCpp(
+    model_path=model_path_str,
+    n_gpu_layers=-1,
+    n_batch=16,
+    n_ctx=8192,
+    temperature=0.75,
+    max_tokens=2500,
+    top_p=1,
+)
+
+templates = rag.load_templates(templates_path)
+embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2", model_kwargs={'device': 'cuda'})
+vector_store = rag.create_vector_store(embeddings, templates)
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=200)
+rag_chain = rag.create_chain(vector_store, model, text_splitter)
+
+print("Invoking RAG chain...")
+response = rag_chain.invoke({"agreement_to_convert": rental_agreement}, {
+                            "recursion_limit": 100})
+print("Response is generated")
+
+print(response["answer"])
